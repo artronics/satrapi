b3122133d21345fb400a4da2119da36486123d85
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-26 19:12:54 +0100

    change app so that it doesn't scan all packages. for this i delete SpringApplicationBoot and use apropriate annotations

--- src/main/java/com/artronics/satrapi/SatrapiApplication.java
+++ src/main/java/com/artronics/satrapi/SatrapiApplication.java
@@ -2,10 +2,20 @@ package com.artronics.satrapi;
 
 import com.artronics.satrapi.persistence.PersistenceConfig;
 import org.springframework.boot.Banner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages = {
+        "com.artronics.satrapi.persistence",
+        "com.artronics.satrapi.core"
+})
+@EnableWebMvc
 public class SatrapiApplication {
 
     public static void main(String[] args) {
