90f956327d70086348f4aa80b29a7b395af3da59
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-26 19:34:44 +0100

    add satrapi initializer and its tes from pre design. it also looks for deed value.

---
+++ src/main/java/com/artronics/satrapi/core/SatrapiInitializer.java
@@ -0,0 +1,72 @@
+package com.artronics.satrapi.core;
+
+import com.artronics.satrapi.entities.SdwnNetwork;
+import com.artronics.satrapi.persistence.repositories.SdwnNetworkRepo;
+import com.artronics.satrapi.persistence.seed.Seeder;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class SatrapiInitializer implements
+                                ApplicationListener<ContextRefreshedEvent>
+{
+    private final static Logger log = Logger.getLogger(SatrapiInitializer.class);
+
+    private String ip;
+    private String connectionString;
+
+    private boolean seedDb=false;
+
+    private SdwnNetwork sdwnNetwork;
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    SdwnNetworkRepo networkRepo;
+
+    @Autowired
+    Seeder seeder;
+
+    @PostConstruct
+    public void initBean(){
+        String seed = "com.artronics.satrapi.persistence.seed";
+        if(!env.containsProperty(seed))
+            seedDb = false;
+        else {
+            seedDb = Boolean.parseBoolean(env.getProperty(seed));
+            connectionString = env.getProperty("com.artronics.satrapi.connection.connection_string");
+        }
+
+        ip = env.getProperty("com.artronics.satrapi.network.ip");
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event)
+    {
+        if (seedDb)
+            seeder.seedNetwork(ip,connectionString);
+
+        initSdwnNetwork();
+
+    }
+
+    public void initSdwnNetwork()
+    {
+        log.info("Initializing SDWN Network with IP: " + ip);
+        sdwnNetwork = networkRepo.findByIp(ip);
+
+        if (sdwnNetwork == null) {
+            log.debug("There is no SDWN-Network on data base. Create on.");
+
+            sdwnNetwork = new SdwnNetwork(ip);
+            networkRepo.save(sdwnNetwork);
+        }
+    }
+}
