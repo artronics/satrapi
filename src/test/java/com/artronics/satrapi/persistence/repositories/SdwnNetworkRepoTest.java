ff114fc08e3a148fd1dc477961c7e0dadcb9f59d
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-26 17:50:30 +0100

    add helpers to test

--- src/test/java/com/artronics/satrapi/persistence/repositories/SdwnNetworkRepoTest.java
+++ src/test/java/com/artronics/satrapi/persistence/repositories/SdwnNetworkRepoTest.java
@@ -1,26 +1,26 @@
 package com.artronics.satrapi.persistence.repositories;
 
+import com.artronics.satrapi.entities.SdwnNetwork;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {
-        RepositoryConfigTest.class,
-//        SatrapiApplication.class
-})
-@ActiveProfiles("test")
-public class SdwnNetworkRepoTest
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+public class SdwnNetworkRepoTest extends RepoBaseTes
 {
-    @Autowired
-    SdwnNetworkRepo networkRepo;
 
     @Test
     public void it(){
 
+    }
+    @Test
+    public void test_findByIp()
+    {
+        SdwnNetwork persistedNet = networkRepo.findByIp(someIp);
+
+        assertNotNull(persistedNet.getId());
+        assertThat(persistedNet.getIp(), equalTo(someIp));
     }
 
 }
