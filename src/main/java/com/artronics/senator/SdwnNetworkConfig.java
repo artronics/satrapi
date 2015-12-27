df9d3188aca050e23fad88a9696d3e76bc86e21a
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-27 14:46:18 +0100

    add another package on parent which contains separate configs from satrapi. it is senator. add config for this new package

---
+++ src/main/java/com/artronics/senator/SdwnNetworkConfig.java
@@ -0,0 +1,10 @@
+package com.artronics.senator;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SdwnNetworkConfig
+{
+    private final static Logger log = Logger.getLogger(SdwnNetworkConfig.class);
+}
