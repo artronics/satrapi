c7a3f7b63f6b48e6eeecb69125e1e499393c36be
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-28 00:47:19 +0100

    add prop file for test and add them to controllerConfigTest

--- src/test/java/com/artronics/senator/controller/ControllerConfigTest.java
+++ src/test/java/com/artronics/senator/controller/ControllerConfigTest.java
@@ -1,8 +1,12 @@
 package com.artronics.senator.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource({
+        "classpath:senator/controller-test-defaults.properties",
+        "classpath:senator/device-test-defaults.properties"})
 public class ControllerConfigTest
 {
 
