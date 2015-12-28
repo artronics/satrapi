2b8d0f8d385aedfe58e6b19c5079d50cbc8d07a5
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-28 18:56:58 +0100

    add controller package

--- src/main/java/com/artronics/senator/controller/ControllerConfig.java
+++ src/main/java/com/artronics/senator/controller/ControllerConfig.java
@@ -1,10 +1,14 @@
 package com.artronics.senator.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan(basePackages = {
+        "com.artronics.senator.controller"
+})
 @PropertySource({
         "classpath:senator/controller-defaults.properties",
         "classpath:senator/device-defaults.properties"})
